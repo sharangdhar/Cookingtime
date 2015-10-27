33c508769da3803009259e50d298ade336851c23
Elliot Lockerman <elliot@lockerman.info>
2015-10-27 19:16:04 -0400

    Added js calculations for temperature using data from water

---
+++ static html/src/js/calculate.js
@@ -0,0 +1,76 @@
+$(document).ready(function()
+{
+    $('#calc_btn').click(calculate);
+    $('#calc_mass').keypress(function(e)
+    {
+        if(e.which == 13)
+        {
+            calculate();
+        }
+    });
+});
+
+
+function calculate()
+{
+    var mass = parseFloat($('#calc_mass').val());
+    var mass_unit = $('#calc_unit option:selected').val();
+    if(mass_unit === 'lb')
+    {
+        mass = lb_to_kg(mass);
+    }
+    if(mass_unit === 'oz')
+    {
+        mass = oz_to_kg(mass);
+    }
+    
+    // else its already in kg
+    
+    var btn = $('#calc_btn');
+    var constant = parseFloat(btn.attr('data-constant'));
+    var wattage = parseFloat(btn.attr('data-wattage'));
+    
+    var end_temp = 373.15; // boiling in K
+    var state = $('#calc_state option:selected').val();
+    var start_temp = 0;
+    if(state === 'frozen')
+    {
+        start_temp = 255.15; // recomended freezer temp in K
+    }
+    else if (state === 'refrigerated')
+    {
+        start_temp = 274.75; // recomended refrigerator temp in K
+    }
+    else if (state === 'room')
+    {
+        start_temp = 294.15;
+    }
+    var dt = end_temp - start_temp;
+    
+    var sec = (constant * (mass * 1000) * dt) / wattage;
+
+
+    // Add time if frozen
+    if(state == 'frozen')
+    {
+        var enth_fuse = 334; // J/g
+        var extra = (enth_fuse * (mass * 1000)) / wattage; 
+        sec += extra;
+    }
+    
+
+    min = sec / 60;
+    $('#calc_ret').val(min + ' min');
+    
+}
+
+
+function lb_to_kg(lb)
+{
+    return lb * 0.453592;
+}
+
+function oz_to_kg(oz)
+{
+    return oz * 0.0283495;
+}
