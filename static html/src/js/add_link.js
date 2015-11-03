126c09700ab04b33c817c10531f703c2b6577f62
Elliot Lockerman <elliot@lockerman.info>
2015-11-02 22:16:02 -0500

    Static html: add link results overhang page expansion

--- static html/src/js/add_link.js
+++ static html/src/js/add_link.js
@@ -10,6 +10,14 @@ $(document).ready(function()
         var results = $("#add_link_results");
         results.css("width", $("#bulk_sidebar_panel").width());
         $("#add_link_results").show(200);
+        
+        var page = $("#page");
+        var overhang = results.offset().top + results.height() - (page.offset().top + page.height());
+        if(overhang > 0)
+        {
+            page.css("min-height", page.height() + overhang + 50);
+        }
+
     });
     
     $("body").click(function()
