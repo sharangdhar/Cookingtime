845a2b2cceb13eeb8e76bec1f32ebf4d57b3944d
Sharangdhar Chakraborty <sharangdharc@yahoo.com>
2016-01-08 00:16:27 +0530

    Folders restructured

--- config files/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = '/opt/myenv/bin/gunicorn'
-pythonpath = '/opt/myenv/cookingti_git/webapps'
-bind = '127.0.0.1:8001'
-workers = 3
--- specification/README.md
+++
@@ -1,2 +0,0 @@
-Turn in your project specification, and any related documents, to
-this directory.
